e2135b1343051825ba5e56f8f643cd2d06946529
Michael  Shear <114257057+MikeJShear@users.noreply.github.com>
2024-01-30 21:15:26 -0800

    SolarSystem_DifferentSpeeds

--- Solar_System/Assets/Scripts/Rotator.cs
+++ Solar_System/Assets/Scripts/Rotator.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-
     public float yawDegreePerSecond = 45f;
+    public GameObject planet;
+    public float orbit;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +19,8 @@ public class Rotator : MonoBehaviour
     {
         Transform myTransform = GetComponent<Transform>();
         myTransform.Rotate(new Vector3(0f,yawDegreePerSecond * Time.deltaTime,0f),Space.World);
+       
+        transform.RotateAround(planet.transform.position, Vector3.up, orbit* Time.deltaTime);
+
     }
 }
